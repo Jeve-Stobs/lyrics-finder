e71a30584a70e790aabf9b5c4bf6fbf7bfb85065
Jeve-Stobs <sam@jevestobs.dev>
2022-07-25 01:43:17 -0400

    🧪 fix failing test

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ npm install @jeve/lyrics-finder
 ```js
 const lyricsFinder = require("@jeve/lyrics-finder");
 
-lyricsFinder.LyricsFinder("sacrifice - the weeknd").then((data) => {
+lyricsFinder.LyricsFinder("blinding lights").then((data) => {
   console.log(data);
 });
 ```
--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 const lyrics = require('../index');
 
 (async () => {
-	const lyric = await lyrics.LyricsFinder('sacrifice - the weeknd');
+	const lyric = await lyrics.LyricsFinder('blinding lights');
 	console.log(lyric);
 })();
 
 // or
 
-lyrics.LyricsFinder('sacrifice - the weeknd').then(data => {
+lyrics.LyricsFinder('blinding lights').then(data => {
 	console.log(data);
 });
